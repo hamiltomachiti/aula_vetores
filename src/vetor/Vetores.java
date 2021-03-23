c3714388b5b80bb2f63c3b2b1f99e1ddc2434bc4
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-03-23 08:27:57 -0300

    Meu primeiro commit

---
+++ src/vetor/Vetores.java
@@ -0,0 +1,24 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		int[] x = new int[4];
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		for(int i = 0; i < x.length;i++)
+		{
+			x[i] = entrada.nextInt();
+		}
+		
+		for(int i = 0; i < x.length; i++)
+		{
+			System.out.println("x["+i+"]="+x[i]);
+		}
+		//0 1 2 3 4		
+	}
+
+}
